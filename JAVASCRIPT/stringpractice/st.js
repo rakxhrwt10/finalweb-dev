1c526004f42f691fc41b283719064fb7ba191b9d
rakeshrawat12 <rakeshrawatr351@gmail.com>
2026-03-28 14:15:23 +0530

    obj

---
+++ JAVASCRIPT/stringpractice/st.js
@@ -0,0 +1,34 @@
+// let arr=["hello","how","are","you"]
+
+
+
+// for(let a=0;a<arr.length;a++){
+
+// console.log(arr[a].toUpperCase());
+
+
+
+// }
+
+let c={
+
+
+    uppercase:function(){
+
+let arr2 =["hello", "how", "are", "you", 1]
+
+for (let a = 0; a < arr.length; a++) {
+
+    if (typeof arr2[a] === "string") {
+        console.log(arr2[a].toUpperCase());
+    } else {
+        console.log(arr2[a]); // number ko waise hi print karo
+    }
+
+}
+
+    }
+
+
+}
+
