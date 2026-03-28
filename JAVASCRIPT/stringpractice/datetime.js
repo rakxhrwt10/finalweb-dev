cdec2d89936326719f7444786756dafaeb6d064c
rakeshrawat12 <rakeshrawatr351@gmail.com>
2026-03-28 14:55:01 +0530

    array

--- JAVASCRIPT/stringpractice/datetime.js
+++ JAVASCRIPT/stringpractice/datetime.js
@@ -1,3 +1,32 @@
-setTimeout(() => {
-  console.log("Time up!");
-}, 1);
+// setTimeout(() => {
+//   console.log("Time up!");
+// }, 1);
+
+//year,month,date,hours,minutes,seconds,milliseconds
+
+let datt=new Date(2025,2,20,12,50,44,50)
+
+console.log(datt.toDateString());
+
+
+//datemethod
+
+console.log(datt.getDate());
+
+console.log(datt.getDay())
+console.log(datt.getHours());
+
+
+
+const now=Date.now(
+
+
+)
+
+const date =new Date(0)
+
+console.log(now);
+console.log(date.toDateString());
+
+
+
