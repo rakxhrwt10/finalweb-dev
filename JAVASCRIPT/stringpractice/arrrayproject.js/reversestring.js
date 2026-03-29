b4f179c319bffa6d7065d814b600ac06107e53b7
rakeshrawat12 <rakeshrawatr351@gmail.com>
2026-03-29 21:38:28 +0530

    string

---
+++ JAVASCRIPT/stringpractice/arrrayproject.js/reversestring.js
@@ -0,0 +1,13 @@
+function reverseString(str) {
+    let reversed = "";
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i];
+    }
+
+    return reversed;
+}
+
+// test
+let str = "hello";
+console.log(reverseString(str));
