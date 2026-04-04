339a821196a72021e62579e253cce0f7907b8680
rakeshrawat12 <rakeshrawatr351@gmail.com>
2026-04-04 23:05:57 +0530

    string

---
+++ JAVASCRIPT/stringpractice/st2.js
@@ -0,0 +1,11 @@
+let header = "Template Strings";
+let tags = ["template strings", "javascript", "es6"];
+
+let html = `<h2>${header}</h2><ul>`;
+for (const x of tags) {
+  html += `<li>${x}</li>`;
+}
+
+html += `</ul>`;
+
+console.log(html);
