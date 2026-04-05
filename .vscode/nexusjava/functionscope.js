5b41be5a1a6d38fc9fee16e08a280f2b03b7e078
rakeshrawat12 <rakeshrawatr351@gmail.com>
2026-04-05 14:11:08 +0530

    arrowthis

--- .vscode/nexusjava/functionscope.js
+++ .vscode/nexusjava/functionscope.js
@@ -2,14 +2,15 @@
 let a = 300
 if (true) {
     let a = 10
+    
     const b = 20
-    // console.log("INNER: ", a);
+    console.log("INNER: ", a);
     
 }
 
 
 
-// console.log(a);
+console.log(a);
 // console.log(b);
 // console.log(c);
 
@@ -27,13 +28,13 @@ function one(){
 
 }
 
-// one()
+one()
 
 if (true) {
     const username = "hitesh"
     if (username === "hitesh") {
         const website = " youtube"
-        // console.log(username + website);
+        console.log(username + website);
     }
     // console.log(website);
 }
@@ -41,18 +42,19 @@ if (true) {
 // console.log(username);
 
 
-// ++++++++++++++++++ interesting ++++++++++++++++++
+// // ++++++++++++++++++ interesting ++++++++++++++++++
 
 
-console.log(addone(5))
+// console.log(addone(5))
 
-function addone(num){
-    return num + 1
-}
+// function addone(num){
+//     return num + 1
+// }
 
 
 
-addTwo(5)
-const addTwo = function(num){
-    return num + 2
-}
+
+// const addTwo = function(num){
+//     return num + 2
+// }
+// console.log(addTwo(5))
